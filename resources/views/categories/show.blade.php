d80a12c6fa5885af7ba5e7f85a961734f7bb2ffc
GaBPC <gabprietoyt@gmail.com>
2017-01-31 17:32:28 -0300

    Mejorada de categories.show
    Ahora se muestran las imagenes de los productos que coinciden con la categoria, ademas de un link a dicho producto

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,48 +1,49 @@
 @extends('layout')
 
-@section('title', "$category->name")
+@section('title', "$category->category_name")
 
 @section('content')
   <div class="row">
     <div class="col-xs-9">
       <h1>{{ $category->name }} Categoria <small>Presente en {{ $category->products()->count() }} producto(s)</small></h1>
     </div>
-    <div class="col-xs-3">
-      {!! Form::open(['route' => ['tags.destroy',$category->id], 'method' => 'DELETE']) !!}
-        {{ Form::submit('Eliminar',['class' => 'btn btn-danger btn-block pull-right', 'style' => 'margin-top: 30px']) }}
-      {!! Form::close() !!}
-    </div>
   </div>
   <hr>
+
+  {{-- Start of row --}}
   <div class="row">
     <div class="col-xs-12">
-      <table class="table">
-        <thead>
-          <th>#</th>
-          <th>Nombre</th>
-          <th>Precio</th>
-          <th>Categoria</th>
-          <th>Otros Tags</th>
-          <th></th>
-        </thead>
-
-        <tbody>
-          @foreach ($category->products as $product)
-            <tr>
-              <th>{{ $product->id }}</th>
-              <td>{{ $product->name }}</td>
-              <td>{{ $product->price }}</td>
-              <td>{{ $product->category->category_name }}</td>
-              <td>
-                @foreach ($product->tags as $tag_p)
-                  <a href="{{ route('tags.show', $tag_p->id) }}"><span class="label label-default">{{ $tag_p->name }}</span></a>
-                @endforeach
-              </td>
-              <td><a href="{{ route('products.show',$product->id) }}" class="btn btn-sm btn-default btn-block">Ver</a></td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
+  @foreach ($category->products as $index => $product)
+    <div class="col-xs-12 col-md-3">
+      <div class="panel-group text-center">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <strong><h3>{{ $product->name }}</h3></strong>
+          </div>
+          <div class="panel-body">
+            @if ($product->image != null)
+              <center>
+                <img class="img-responsive" src="{{asset('images/' . $product->image)}}" alt="Imagen para {{ $product->name }}">
+              </center>
+            @endif
+            <strong>Precio unitario: </strong>${{ $product->price }}
+            <br>
+            <strong> ID: </strong>{{ $product->id }}
+            <br>
+            <hr>
+            <div class="col-xs-12">
+              <a href="{{route('products.show', $product->id)}}" class="btn btn-info btn-sm btn-block">Más información</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+    {{-- For md screen: if 4 items have already been printed, closes the row and open a new one --}}
+    @if (($index + 1) % 4 == 0)
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+    @endif
+  @endforeach
 @endsection
