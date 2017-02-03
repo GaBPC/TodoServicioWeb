3571d22fd3330e3df4db687e9ce42f2efa925b8a
GaBPC <gabprietoyt@gmail.com>
2017-02-03 00:54:39 -0300

    Seeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,15 +11,13 @@ class ProductsTableSeeder extends Seeder
   */
   public function run()
   {
-    for ($i=0; $i < 15; $i++) {
-      DB::table('products')->insert([
-        'name' => 'Producto '.$i,
-        'price' => rand(0,1000),
-        'image' => null,
-        'category_id' => rand(1,15),
-        'created_at' => DB::raw('CURRENT_TIMESTAMP'),
-        'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
-      ]);
-    }
+    DB::table('products')->insert([
+      'name' => 'Martillo ',
+      'price' => 45,
+      'image' => null,
+      'category_id' => 1,
+      'created_at' => DB::raw('CURRENT_TIMESTAMP'),
+      'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
+    ]);
   }
 }
